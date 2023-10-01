eb0842ab71ec06c8089ea09cdb13dfd3e74747c1
natnaelkassahun <88572912+natnaelkassahun@users.noreply.github.com>
2023-10-01 03:51:34 +0300

    Update CategoryView.jsx

--- frontend/src/routes/CategoryView.jsx
+++ frontend/src/routes/CategoryView.jsx
@@ -15,7 +15,7 @@ const CategoryView = () => {
     const [ loading , setLoading ] = useState(true) 
 
     useEffect(() => {
-        axios.get("https://ethio-wekil-backend.vercel.app/items")
+        axios.get("http://ethio-wekil-backend.vercel.app/items")
             .then(res => {
                 setMenItems(res.data.filter((item) => item.category === "men"))
                 setKidsItems(res.data.filter((item) => item.category === "kids" ))
