ece8db113c2971cb014de694e8a844a1f6eba439
natnaelkassahun <88572912+natnaelkassahun@users.noreply.github.com>
2023-10-01 23:44:40 +0300

    Update CategoryCard.js

--- frontend/src/components/Card/FeaturedCard/CategoryCard.js
+++ frontend/src/components/Card/FeaturedCard/CategoryCard.js
@@ -13,6 +13,7 @@ const CategoryCard = (props) => {
                 <div className="category__card__detail">
                     <div className="product__name">
     
+    
                       
                            {props.data.name}
                        
